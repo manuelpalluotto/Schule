d6fbcab73b32ec4ebdb92eb49d8747e3ce7182ad
Manu <pflaume1337@gmail.com>
2024-09-09 20:23:20 +0200

    bugfix

--- src/whilst.java
+++ src/whilst.java
@@ -1,22 +1,12 @@
 public class whilst {
     public static void main(String[] args) {
-        int sum = 0;
         int counter = 0;
-
+        int sum = 0;
         while (counter < 10) {
             counter++;
-            sum = sum + counter;
-            System.out.println(sum);
-
-
+            sum += counter;
         }
         System.out.println(sum);
-
-        // do {
-
-        //} while (condition)
-
-
     }
 }
 
---
+++ src/whilstfor.java
@@ -0,0 +1,5 @@
+public class whilstfor {
+    public static void main(String[] args) {
+
+    }
+}
